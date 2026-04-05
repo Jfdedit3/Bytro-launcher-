4e3ceb3e217855e440e60500d4c779258d90c149
z4trox <kylian.lefebvre60@gmail.com>
2026-04-05 20:55:11 +0200

    Add game repository

---
+++ app/src/main/java/com/jfdedit3/bytrolauncher/GameRepository.kt
@@ -0,0 +1,10 @@
+package com.jfdedit3.bytrolauncher
+
+object GameRepository {
+    val games = listOf(
+        GameItem("Call of War", "https://www.callofwar.com/", "#C62828"),
+        GameItem("Supremacy 1914", "https://www.supremacy1914.com/", "#5D4037"),
+        GameItem("Conflict of Nations", "https://www.conflictnations.com/", "#1565C0"),
+        GameItem("New World Empires", "https://www.newworldempires.com/", "#2E7D32")
+    )
+}
