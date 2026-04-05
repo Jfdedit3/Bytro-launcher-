38805ec3ee6f4de98c2e888ff8bd9b9569f7718b
z4trox <kylian.lefebvre60@gmail.com>
2026-04-05 20:55:55 +0200

    Add main activity

---
+++ app/src/main/java/com/jfdedit3/bytrolauncher/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.jfdedit3.bytrolauncher
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import com.jfdedit3.bytrolauncher.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = getString(R.string.app_name)
+        supportActionBar?.subtitle = getString(R.string.app_subtitle)
+
+        binding.viewPager.adapter = GamePagerAdapter(this)
+        binding.viewPager.offscreenPageLimit = GameRepository.games.size
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = GameRepository.games[position].title
+        }.attach()
+    }
+}
